3c8eac0497a4e2c3acc4a8bf80a198c62a511387
tanto <tantows001@gmail.com>
2023-10-18 13:48:38 +0700

    Build

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    @vite(['resources/css/app.css','resources/js/app.js'])
+    <link rel="stylesheet" href="{{ asset('/build/assets/app-2c3366f8.css') }}">
 </head>
 <body>
     @include('partials.navbar')
@@ -13,5 +13,7 @@
     <div class="p-8 lg:px-32">
         @yield('content')
     </div>
+
+    <script src="{{ asset('/build/assets/app-ac13ce32.js') }}"></script>
 </body>
 </html>
